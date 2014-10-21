98f6e889f0e51b0c5d0251cdf1056c890c9be95f
Halaalmargi <halaalmargi@gmail.com>
2014-10-20 21:39:47 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -13,6 +13,18 @@
 
 -(void)update:(CCTime)delta
 {
+    // Increment the time since the last obstacle was added
+    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
+    
+    // Check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // Then reset the timer.
+        timeSinceObstacle = 0.0f;
+    }
     // this will be run every frame.
     // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
     
@@ -24,19 +36,7 @@
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     [self addObstacle];timeSinceObstacle = 0.0f;
     // this will get called every time the player touches the screen
-    // Increment the time since the last obstacle was added
-    timeSinceObstacle += delta;
-    // delta is approximately 1/60th of a second
     
-    // Check to see if two seconds have passed
-    if (timeSinceObstacle > 2.0f)
-    {
-        // Add a new obstacle
-        [self addObstacle];
-        
-        // Then reset the timer.
-        timeSinceObstacle = 0.0f;
-    }
     [character flap];
 }
 
